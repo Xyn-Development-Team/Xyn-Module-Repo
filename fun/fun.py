57529f03e41e3b5ed53e7a46a91c9e4538483348
Moonlight Dorkreamer <moonlightdorkreamer@gmail.com>
2024-04-12 18:52:02 -0300

    Fun module
    Lot's of fun commands!

---
+++ fun/fun.py
@@ -0,0 +1,316 @@
+class module:
+    name = "Fun"
+    cog_name = "fun"
+    description = "Lot's of fun commands!"
+    author = "Moonlight Dorkreamer 🌓"
+    xyn_version = "V3"
+    version = "12042024"
+
+import os
+
+if __name__ == "__main__":
+    # This is supposed to be imported by Xyn! You dummy!!!
+    # We can't really localize this, since loading localization with this being directly executed would crash :c
+    print("This is supposed to be imported by Xyn! You dummy!!!")
+    os._exit(1)
+else:
+    import discord
+    from discord import app_commands
+    from discord.ext import commands
+    import random
+    import requests
+    import storage
+    from typing import Optional, Union
+    import modules.fun.imagetools as imagetools
+    import localization
+    import re
+    from uwuipy import uwuipy
+
+class Confession(discord.ui.Modal,title="Confession"):
+    user_title = discord.ui.TextInput(
+        label="Title",
+        placeholder="A nice eye-catching title",
+        max_length=40
+    )
+
+    confession = discord.ui.TextInput(
+        label="Confession",
+        placeholder="Your confession...",
+        style=discord.TextStyle.long
+    )
+
+    async def on_submit(self, interaction: discord.Interaction):
+        embed = discord.Embed(
+            description=self.confession.value,
+            color=discord.Color.random(),
+        )
+        confessions = storage.guild.read(interaction.guild_id,option="confessions")
+
+        if confessions:
+            confessions = int(confessions) + 1
+        else:
+            confessions = 1
+        storage.guild.set(interaction.guild_id,"confessions",confessions)
+
+        embed.title = f"Confession #{confessions} {self.user_title.value}"
+        await interaction.response.send_message(localization.external.read("confession_sent",storage.guild.read(interaction.guild.id, "language")),ephemeral=True)
+        await interaction.channel.send(embed=embed)
+
+class fun(commands.GroupCog, name=module.cog_name):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+        super().__init__()
+
+        bot.tree.add_command(app_commands.ContextMenu(name="Quote this!", callback=self.quote_menu))
+
+    async def quote_menu(self, interaction: discord.Interaction, message: discord.Message):
+        await interaction.response.defer(thinking=True)
+        if interaction.guild:
+            language = storage.guild.read(interaction.guild.id,"language")
+        else:
+            language = str(interaction.locale).lower()
+        image = imagetools.quote(message.author.id, message.author.name, message.author.display_name, message.author.display_avatar.url, message.content)
+        if image:
+            await interaction.followup.send(file=discord.File(image))
+            os.remove(image)
+        else:
+            # An error has occurred trying to generate this image :c
+            await interaction.followup.send(localization.external.read("image_gen_error", language))        
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload:discord.RawReactionActionEvent):
+        emoji = payload.emoji
+        guild = self.bot.get_guild(payload.guild_id)
+        channel = guild.get_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+
+        for reaction in message.reactions:
+            if reaction.emoji == storage.guild.read(guild.id, "starboard_emoji") and reaction.count >= int(storage.guild.read(guild.id, "starboard_threshold")):
+                starboard_channel = guild.get_channel(storage.guild.read(guild.id, "starboard_channel"))
+                
+                embed = discord.Embed(title=message.author.display_name,description=f"\"{message.content}\"", color=discord.Color.from_str(imagetools.get_average_color(message.author.display_avatar.url))).set_thumbnail(url=message.author.display_avatar.url)
+
+                attachment_counter = 0
+                attachments = []
+                
+                for attachment in message.attachments:
+                    attachment_counter =+ 1
+                    attachments.append(attachment.url)
+                
+                if attachment_counter == 1:
+                    embed.set_image(url=attachments[0])
+
+                await starboard_channel.send(embed=embed)
+
+                if attachment_counter > 1:
+                    for attachment in attachments:
+                        await starboard_channel.send(attachment)
+
+    #/diceroll
+    @app_commands.command(name="diceroll",description="Allows you to roll a dice")
+    @app_commands.describe(sides="How many sides do you want the dice to have, you can go up to 999 sides!")
+    async def diceroll(self, interaction: discord.Interaction,sides:app_commands.Range[int,1,999]):
+        dice = random.randint(1,int(sides))
+        image = imagetools.dice(interaction.user.id,dice)
+
+        thumbnail = discord.File(image,filename=f"dice_{dice}_xyn.png")
+
+        # Title = **{user}**
+        # Description = has rolled a **{dice}!**
+        embed = discord.Embed(title=f"**{interaction.user.display_name}**",description=str(localization.external.read("diceroll_embed_description",storage.guild.read(interaction.guild.id,"language"))).format(dice=dice),color=discord.Color.from_str("#e8439c"))
+        embed.set_thumbnail(url=f"attachment://dice_{dice}_xyn.png")
+        
+        await interaction.response.send_message(file=thumbnail,embed=embed)
+        thumbnail.close()
+        os.remove(image)
+
+    #TODO: Add a coin thumbnail to the embed
+    #/coinflip
+    @app_commands.command(name="coinflip",description="Allows you to flip a coin")
+    async def coinflip(self, interaction: discord.Interaction):
+        coin = random.randint(0,1)
+        if coin < 1:
+            result = localization.external.read("coinflip_heads",storage.guild.read(interaction.guild.id,"language"))
+        else:
+            result = localization.external.read("coinflip_tails",storage.guild.read(interaction.guild.id,"language"))
+        # **{user}** has flipped **{result}!**
+        embed = discord.Embed(title=str(localization.external.read("coinflip_embed_title",storage.guild.read(interaction.guild.id,"language"))).format(user=interaction.user.display_name, result=result))
+        await interaction.response.send_message(embed=embed)
+    
+    #/uselessfacts
+    # TODO Implement Google Translate into the response in case the guild language isn't en-us
+    @app_commands.command(name="uselessfacts",description="Gives you a random useless fact about za warudo")
+    async def uselessfacts(self, interaction: discord.Interaction):
+        fact = requests.get("https://useless-facts.sameerkumar.website/api").json()["data"]
+        await interaction.response.send_message(fact)
+
+    #/local_persona
+    @app_commands.command(name="local_persona", description="Allows you to send a message as another fellow user")
+    @app_commands.describe(message="What do you want them to say?", user="Who do you want to say it?", media="Do you want them to also send something?")
+    async def local_persona(self, interaction:discord.Interaction, message:str, user:discord.User, media:Optional[discord.Attachment]):
+        #DM's
+        if not interaction.guild:
+            # This command can only be used within a guild!
+            return await interaction.response.send_message(localization.internal.read("guild_only",storage.guild.read(interaction.guild.id,"language")))
+        
+        await interaction.response.send_message(str(localization.internal.read("thinking",storage.guild.read(interaction.guild.id,"language"))).format(bot=interaction.client.user.display_name), ephemeral=True)
+
+        webhook = await interaction.channel.create_webhook(name=user.display_name,avatar=requests.get(user.display_avatar.url).content)
+        await webhook.send(message)
+        
+        if media:
+            await webhook.send(media.url)
+        await webhook.delete()
+
+    #/persona
+    @app_commands.command(name="persona",description="Allows you to send a message as \"another user\"")
+    @app_commands.describe(message="What do you want your persona to say?", username="What's the name of your persona?", pfp="How does your persona look like?", media="Do you want to also send an attachment in the chat?")
+    async def persona(self, interaction: discord.Interaction, message:str, username:Optional[str], pfp:Optional[discord.Attachment], media:Optional[discord.Attachment]):
+        #DM's
+        if not interaction.guild:
+            # This command can only be used within a guild!
+            return await interaction.response.send_message(localization.internal.read("guild_only",storage.guild.read(interaction.guild.id,"language")))
+        
+        if not username:
+            username = storage.user.read(interaction.user.id,"persona_username")
+            if not username:
+                #You need to provide an username at least once to use this command!
+                return await interaction.response.send_message(localization.external.read("persona_no_username"))
+        else:
+            storage.user.set(interaction.user.id,"persona_username",username)
+
+        # {bot} is thinking...
+        await interaction.response.send_message(str(localization.internal.read("thinking",storage.guild.read(interaction.guild.id,"language"))).format(bot=interaction.client.user.display_name), ephemeral=True)
+
+        if not pfp:
+            pfp = storage.user.read(interaction.user.id,"persona_pfp")
+            
+            if pfp:
+                webhook = await interaction.channel.create_webhook(name=username,avatar=requests.get(pfp).content)
+            else:
+                webhook = await interaction.channel.create_webhook(name=username)
+        else:
+            storage.user.set(interaction.user.id,"persona_pfp",pfp)
+            webhook = await interaction.channel.create_webhook(name=username,avatar=requests.get(pfp.url).content)
+
+        await webhook.send(message)
+        await webhook.delete()
+
+    #/quote
+    @app_commands.command(name="quote",description="Someone said something quite poetic, huh?")
+    @app_commands.describe(quote="What did they say?", user="Who here was the poetic menace?", name="How's the poetic menace called?", pfp="How does the poetic menace look?")
+    async def quote(self, interaction: discord.Interaction, quote:str, user:Optional[discord.User],name:Optional[str],pfp:Optional[discord.Attachment]):
+        await interaction.response.defer(thinking=True)
+        if user:
+            image = imagetools.quote(user.id, user.name, user.display_name, user.display_avatar.url, quote)
+        else:
+            image = imagetools.quote(id=interaction.user.id, display_name=name if name else None, pfp=pfp if pfp else None,quote=quote)
+        
+        if image:
+            await interaction.followup.send(file=discord.File(image))
+            os.remove(image)
+        else:
+            # An error has occurred trying to generate this image :c
+            await interaction.followup.send(localization.external.read("image_gen_error",storage.guild.read(interaction.guild.id,"language")))
+
+    #/confess
+    @app_commands.command(name="confess",description="Allows you to confess anonymously")
+    async def confess(self, interaction: discord.Interaction):
+        if interaction.guild:
+            await interaction.response.send_modal(Confession())
+        else:
+            # This command can only be used within a guild!
+            await interaction.response.send_message(localization.internal.read("guild_only",storage.guild.read(interaction.guild.id,"language")))
+
+    #/starboard_setup
+    @app_commands.command(name="starboard_setup",description="Setup how starboards behave")
+    @app_commands.describe(channel="Where are starboard messages getting reposted to?", emoji="What emoji will count as a vote? (external emojis are allowed!)",threshold="How many votes until a message goes to the starboard channel?")
+    @app_commands.default_permissions(manage_messages=True)
+    async def starboard_setup(self, interaction:discord.Interaction, channel:discord.TextChannel, emoji:str, threshold:int):
+        await interaction.response.defer(thinking=True,ephemeral=True)
+
+        emoji_pattern = re.compile(r'<:\w+:\d+>|[^\w\s,]')
+        emojis = emoji_pattern.findall(emoji)
+
+        storage.guild.set(interaction.guild_id, "starboard_channel", channel.id)
+        storage.guild.set(interaction.guild_id, "starboard_emoji", emojis[0])
+        storage.guild.set(interaction.guild_id, "starboard_threshold", threshold)
+
+        await interaction.followup.send(localization.external.read("starboard_setup", storage.guild.read(interaction.guild.id, "language")),ephemeral=True)
+
+    #/uwuify
+    @app_commands.command(name="uwuify", description="Makes everything you say KAWAII!!!")
+    @app_commands.describe(say="what do you wannya say?!!")
+    async def uwuify(self, interaction: discord.Interaction, say:str):
+        await interaction.response.send_message(str(localization.internal.read("thinking")).format(bot=self.bot.user.display_name),ephemeral=True)
+        uwu = uwuipy()
+
+        webhook = await interaction.channel.create_webhook(name=interaction.user.display_name,avatar=requests.get(interaction.user.display_avatar.url).content)
+        await webhook.send(uwu.uwuify(say))
+        await webhook.delete()
+
+    #/rip
+    @app_commands.command(name="rip", description="Makes an image of a tombstone")
+    @app_commands.describe(name="Who died?", description="Something to go at the bottom of their tombstone")
+    async def rip(self, interaction: discord.Interaction, name:Optional[str], description:Optional[str], pfp:Optional[discord.Attachment], user:Optional[discord.User]):
+        await interaction.response.defer(thinking=True)
+        
+        if user:
+            image = imagetools.rip(interaction.user.id, user.display_name, description, user.display_avatar.url)
+        else:
+            image = imagetools.rip(interaction.user.id, name, description, pfp.url if pfp else None)
+
+        if image:
+            await interaction.followup.send(file=discord.File(image))
+            os.remove(image)
+        else:
+            # An error has occurred trying to generate this image :c
+            await interaction.followup.send(localization.external.read("image_gen_error",storage.guild.read(interaction.guild.id,"language")))
+
+    #/love
+    @app_commands.command(name="love", description="A \"totally accurate\" love meter")
+    @app_commands.describe(person1="Someone or something on slot 1!")
+    @app_commands.describe(person2="Someone or something on slot 2!")
+    @app_commands.describe(user1="A user for slot 1!")
+    @app_commands.describe(user2="A user for slot 2!")
+    async def love(self, interaction: discord.Interaction, person1: Optional[str], person2: Optional[str], user1: Optional[discord.User], user2: Optional[discord.User]):
+        if user1:
+            person1 = user1.display_name
+        if user2:
+            person2 = user2.display_name
+
+        if not person1 or not person2:
+            return await interaction.response.send_message(localization.external.read("love_none", storage.guild.read(interaction.guild.id, "language")))
+
+        # Use the full length names for the first seed.
+        random.seed(person1.lower() + person2.lower())
+
+        # This should make sure longer/shorter names don't create biases.
+        seed_length = 5
+
+        # Trim both seeds.
+        seed_person1 = person1[:seed_length].lower()
+        seed_person2 = person2[:seed_length].lower()
+
+        # Pad the shorter seed with spaces to make their lengths equal
+        seed_person1 = seed_person1.ljust(seed_length)
+        seed_person2 = seed_person2.ljust(seed_length)
+
+        total = 0
+        for seed in (seed_person1, seed_person2):
+            for character in seed:
+                points = random.uniform(0, 1)
+                total += points
+        total = round(total) * 10
+        total = min(max(total, 0), 100)
+
+        embed = discord.Embed(title=f"{person1} :pink_heart: {person2}", description=f"{total}%", color=discord.Color.from_str("#e8439c"))
+
+        await interaction.response.send_message(embed=embed)
+
+
+async def setup(bot: commands.Bot) -> None:
+    if not os.path.isdir("./modules/fun/temp"):
+        os.makedirs("./modules/fun/temp")
+    print("fun.py was loaded!")
+    await bot.add_cog(fun(bot))
---
+++ fun/imagetools.py
@@ -0,0 +1,163 @@
+from PIL import Image, ImageDraw, ImageFont, ImageFilter, ImageEnhance
+import textwrap
+import requests
+import urllib
+import time
+from io import BytesIO
+import re
+from typing import Union
+
+def get_image(image, resize=False, size=(300, 300)):
+    """Checks if the image is a URL and if so, loads it locally, else it tries to load from a path.
+    If `resize` is True, it will resize the image to the specified `size`."""
+    
+    if "https://" in image:
+        req = urllib.request.Request(image, headers={'User-Agent': 'Mozilla/5.0'})
+        with urllib.request.urlopen(req) as response:
+            image = Image.open(response)
+    else:
+        image = Image.open(image)
+    
+    if resize:
+        image = image.resize(size)
+    
+    return image
+
+
+def quote(id:Union[int, str], username=None, display_name="Anonymous", pfp=None, quote="") -> str:
+    """Generates an image with a user's quote.
+
+    Args:
+        id (Union[int, str]): The user's Discord ID, used solely for easily distinguishing requests.
+        username (str, optional): The user's Discord @. Defaults to None.
+        display_name (str, optional): The user's display name. Defaults to "Anonymous".
+        pfp (str optional): A URL to the user's pfp. Defaults to None.
+        quote (str, optional): The content you want to quote. Defaults to "".
+
+    Returns:
+        str: Path for the image file.
+    """
+    
+    quote = "\"" + quote + "\""
+    background = Image.new("RGBA",(1280,720))
+    gradient = Image.open("./modules/fun/assets/quote_gradient.png").resize((1280,720)).convert(mode="RGBA")
+    font = ImageFont.truetype("./modules/fun/assets/fonts/DejaVuSans.ttf",28) #28
+    font2 = ImageFont.truetype("./modules/fun/assets/fonts/DejaVuSans.ttf",20)
+    font3 = ImageFont.truetype("./modules/fun/assets/fonts/DejaVuSans.ttf",20)
+
+    draw = ImageDraw.Draw(background)
+
+    if pfp:
+        pfp = get_image(pfp, True, (810, 810))
+        background.paste(pfp)
+
+    background.paste(gradient,(0,0),gradient)
+
+    #Quote
+    draw.text((980,140),textwrap.fill(quote,35),None,font,align="left",anchor="ms")
+
+    #Display name
+    draw.text((980,600),textwrap.fill("- " + display_name,45),None,font,align="left",anchor="ms")
+
+    if username:
+        #Username
+        draw.text((980,630),textwrap.fill("@" + username if "@" not in username else username ,45), None, font3, align="left", anchor="ms")
+
+    if __name__ == "__main__":
+        background.show()
+    else:
+        filename = f"{id}_{time.strftime('%d_%m_%Y_%H_%M_%S')}.png"
+        background.save(f"./modules/fun/temp/{filename}")
+        return "./modules/fun/temp/" + filename
+
+def dice(id:Union[int, str], number:int) -> str:
+    """Generates the image of a dice with a random number based on the number of sides
+
+    Args:
+        id (Union[int, str]): The user's Discord ID, used solely for easily distinguishing requests.
+        number (int): The number of sides the dice has from 1 - 999.
+
+    Returns:
+        str: Path for the image file.
+    """
+
+    dice = Image.open("./modules/fun/assets/dice.png").convert(mode="RGBA")
+    draw = ImageDraw.Draw(dice)
+    font = ImageFont.truetype("./modules/fun/assets/fonts/DejaVuSans.ttf",175)
+
+    draw.text((515,500),str(number),align="center",anchor="mm",font=font)
+
+    if __name__ == "__main__":
+        dice.show()
+    else:
+        filename = f"{id}_{time.strftime('%d_%m_%Y_%H_%M_%S')}.png"
+        dice.save(f"./modules/fun/temp/{filename}")
+        return f"./modules/fun/temp/{filename}"
+
+def rip(id:Union[int, str], display_name=str, description=None, pfp=None) -> str:
+    """Generates the image of a tombstone with the user's display name, pfp and a description.
+
+    Args:
+        id (Union[int, str]): The user's Discord ID, used solely for easily distinguishing requests.
+        display_name (str): The user's Discord display name. Defaults to str.
+        description (str, optional): The text what will go under the user's name and pfp on the gravestone. Defaults to None.
+        pfp (str, optional): A URL to the user's pfp. Defaults to None.
+
+    Returns:
+        str: A Path for the image file.
+    """
+
+    tombstone = Image.open("./modules/fun/assets/tombstone.png")
+
+    text_layer = Image.new("RGBA", tombstone.size)
+
+    font = ImageFont.truetype("./modules/fun/assets/fonts/DejaVuSans.ttf",35)
+
+    if pfp:
+        pfp = get_image(pfp, (300, 300))
+        pfp = ImageEnhance.Contrast(pfp).enhance(2)
+        pfp = pfp.filter(ImageFilter.EMBOSS)
+        pfp = ImageEnhance.Color(pfp).degenerate
+        tombstone.paste(pfp, ((450, 170)), pfp)
+
+    text_draw = ImageDraw.Draw(text_layer)
+    text_draw.text((600,125),textwrap.fill(display_name if display_name else "Anonymous",30),fill="white",stroke_fill="black",stroke_width=4,font=font,align="center",anchor="ms")
+    
+    if description:
+        text_draw.text((600,505),textwrap.fill(description,30),fill="white",stroke_fill="black",stroke_width=4,font=font,align="center",anchor="ms")
+    
+    text_layer = text_layer.filter(ImageFilter.EMBOSS)
+    tombstone.paste(text_layer, text_layer)
+
+    tombstone = ImageEnhance.Contrast(tombstone).enhance(2)
+
+    if __name__ == "__main__":
+        tombstone.show()
+    else:
+        filename = f"{id}_{time.strftime('%d_%m_%Y_%H_%M_%S')}.png"
+        tombstone.save(f"./modules/fun/temp/{filename}")
+        return f"./modules/fun/temp/{filename}"
+
+def rgb_to_hex(rgb):
+    return "#{:02x}{:02x}{:02x}".format(rgb[0], rgb[1], rgb[2])
+
+def get_average_color(image):
+    image = get_image(image)
+    
+    # Convert the image to RGBA if it's not already
+    if image.mode != 'RGBA':
+        image = image.convert('RGBA')
+
+    # Convert the image to a numpy array for faster processing
+    pixels = np.array(image)
+
+    # Calculate the average RGB values
+    avg_color = tuple(np.average(pixels, axis=(0,1)).astype(int))
+
+    return rgb_to_hex(avg_color)
+
+if __name__ == "__main__":
+    id = 69
+    #rip(id, "Moonlight Dorkreamer 🌓", "Was too much of a dork to be left alive! here's some more text to feasten your eyes!", "./modules/fun/temp/pfp.jpg")
+    #dice(id,999)
+    #quote(id, "@dorkreamer", "Moonlight Dorkreamer 🌓",f"./modules/fun/temp/pfp.jpg","I friggin love pudding, anyways here's a long thing OwO, how about we make this even longer and much more lovely, I think we just done it, it's properly wrapping rn")
